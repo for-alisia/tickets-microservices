9df409ae6fce278e66280005752ac50af72e5517
Elena <for-alisia@yandex.ru>
2023-02-02 17:03:00 +0100

    Reverted back to local dev cluster

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.post('/api/users/signup', (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || typeof email !== 'string') {
+    res.status(400).send('Provide a valid email');
+  }
   res.send('Sign in service');
 });
 
